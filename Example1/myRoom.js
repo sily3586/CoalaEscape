b511b6db677353121bc39cee6b448bc9a64ae2d9
Hyeok-min <sily3586@gmail.com>
2019-09-24 04:47:40 +0900

    기존 예시 파일 삭제

--- Example1/myRoom.js
+++
@@ -1,4 +0,0 @@
-﻿room = game.createRoom("room", "배경-1.png") // 방 생성
-
-game.start(room) // 게임시작
-printMessage("방탈출에 오신 것을 환영합니다!") // 환영 메시지 출력
--- Example1/samplecode.js
+++
@@ -1,4 +0,0 @@
-﻿room = game.createRoom("room", "배경-1.png") // 방 생성
-
-game.start(room) // 게임시작
-printMessage("환영합니다!")
